8a24d518b98d4f71e4f244a9a3e91932cf2bd0bb
Ashank007 <guptaashank38@gmail.com>
2025-04-26 18:00:54 +0530

    Added Floating Terminal Support

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,41 +1,53 @@
 return {
-	"akinsho/toggleterm.nvim",
-	config = function()
-		require("toggleterm").setup({
-			size = 10, -- Adjust the terminal height
-			open_mapping = [[<C-\>]],
-			direction = "horizontal", -- Opens at the bottom
-			start_in_insert = true,
-			persist_mode = false, -- Prevents closing when you switch buffers
-		})
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require("toggleterm").setup({
+      size = 4,
+      open_mapping = [[<C-\>]],
+      direction = "float",
+      start_in_insert = true,
+      persist_mode = false,
+      float_opts = {
+        border = "rounded",
+        width = 120,
+        height = 30,
+      }
+    })
 
-		-- Remove line numbers and cursorline in terminal mode
-		vim.api.nvim_create_autocmd("TermOpen", {
-			pattern = "*",
-			callback = function()
-				vim.cmd("setlocal nonumber norelativenumber nocursorline")
-			end,
-		})
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "*",
+      callback = function()
+        vim.cmd("setlocal nonumber norelativenumber nocursorline")
+      end,
+    })
 
-		-- Keybindings
-		local opts = { noremap = true, silent = true }
+    local opts = { noremap = true, silent = true }
 
-		-- Exit terminal mode with Esc
-		vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", opts)
+    vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", opts)
+    vim.api.nvim_set_keymap("n", "<C-t>", ":ToggleTerm<CR>", opts)
+    vim.api.nvim_set_keymap("t", "<C-t>", "<C-\\><C-n>:ToggleTerm<CR>", opts)
 
-		-- Jump between terminal and normal buffer
-		vim.api.nvim_set_keymap("n", "<C-t>", ":ToggleTerm<CR>", opts) -- Open/close terminal
-		vim.api.nvim_set_keymap("t", "<C-t>", "<C-\\><C-n>:ToggleTerm<CR>", opts) -- Exit & toggle
+    vim.api.nvim_set_keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", opts)
+    vim.api.nvim_set_keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", opts)
+    vim.api.nvim_set_keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", opts)
+    vim.api.nvim_set_keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", opts)
 
-		-- Move between windows easily
-		vim.api.nvim_set_keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", opts)
-		vim.api.nvim_set_keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", opts)
-		vim.api.nvim_set_keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", opts)
-		vim.api.nvim_set_keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", opts)
+    vim.api.nvim_set_keymap("t", "<C-Up>", "<C-\\><C-n><C-w>k", opts)
+    vim.api.nvim_set_keymap("t", "<C-Down>", "<C-\\><C-n><C-w>j", opts)
 
-		-- Move up/down in terminal while in insert mode
-		vim.api.nvim_set_keymap("t", "<C-Up>", "<C-\\><C-n><C-w>k", opts)
-		vim.api.nvim_set_keymap("t", "<C-Down>", "<C-\\><C-n><C-w>j", opts)
-	end,
+    -- === Custom Terminals ===
+    local Terminal = require('toggleterm.terminal').Terminal
+
+    local float_term = Terminal:new({ direction = "float", hidden = true })
+
+    local vertical_term = Terminal:new({ direction = "vertical", size = 80, hidden = true })
+    function _VERTICAL_TERM_TOGGLE()
+      vertical_term:toggle()
+    end
+
+    -- Custom toggles
+    vim.api.nvim_set_keymap('n', '<leader>tv', '<cmd>lua _VERTICAL_TERM_TOGGLE()<CR>', opts)
+  end
 }
+
 
